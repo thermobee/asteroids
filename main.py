dfa32da13fb6281019e171c6a022ef702f9d9d8b
Tod Lazarov <tod.lazarov@gmail.com>
2025-08-20 11:47:06 -0400

    major commit
    The canvas is working, limited to 60fps. The player movement is working smoothly as well.

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from constants import *
+from player import Player
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
+        pygame.display.flip()
+
+        # Limit to 60 fps
+        dt = clock.tick(60) / 1000
+
+if __name__ == "__main__":
+    main()
